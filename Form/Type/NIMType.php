393a83d88aaee54952d688a2921eb804cc6abd37
alanglade <alanglade@galilee.fr>
2013-09-03 13:54:22 +0200

    Adding NiM form type

---
+++ Form/Type/NIMType.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the NIM package.
+ *
+ * (c) Langlade Arnaud
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace  NIM\Component\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+abstract class NIMType extends AbstractType
+{
+    /**
+     * Constructor.
+     *
+     * @param string $dataClass
+     * @param array $validationGroups
+     */
+    public function __construct($dataClass, array $validationGroups)
+    {
+        $this->dataClass = $dataClass;
+        $this->validationGroups = $validationGroups;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'data_class' => $this->dataClass,
+                'validation_groups' => $this->validationGroups,
+            ))
+        ;
+    }
+}
